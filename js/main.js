3991c9fe5e75d5cb3a5b1fa8692a645ea4bbb451
Chris Kwan <chriswkwan@gmail.com>
2016-03-26 18:34:25 -0400

    Remove test files: main and testmodule

--- js/main.js
+++
@@ -1,5 +0,0 @@
-import tester from './testmodule';
-
-export let there = "light";
-
-alert(tester.greeting());
--- js/testmodule.js
+++
@@ -1,5 +0,0 @@
-console.log("I will say hello");
-
-export default function greeting() {
-    return "hello!";
-}
