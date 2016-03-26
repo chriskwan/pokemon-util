8c5eb8a98fce7bc7cc8ee07ba87c272ad42e8199
Chris Kwan <chriswkwan@gmail.com>
2016-03-26 18:35:22 -0400

    Rename random-pokemon to main

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -42,40 +42,96 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	exports.there = undefined;
-
-	var _testmodule = __webpack_require__(1);
-
-	var _testmodule2 = _interopRequireDefault(_testmodule);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	var there = exports.there = "light";
-
-	alert(_testmodule2.default.greeting());
-
-/***/ },
-/* 1 */
 /***/ function(module, exports) {
 
 	"use strict";
 
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-	exports.default = greeting;
-	console.log("I will say hello");
+	//cwkTODO move this code to random-pokemon package
+	(function () {
+	    //cwkTODO generate a pokemon number to name dictionary
+	    //cwkTODO generate a pokemon name to number dictionary
+	    //cwkTODO or have two dictionaries that reference the same objects
 
-	function greeting() {
-	    return "hello!";
-	}
+	    var pokemonApiBasePath = "https://pokeapi.co";
+
+	    var getUrl = function getUrl(url, callback) {
+	        var request = new XMLHttpRequest();
+	        request.onload = callback;
+	        request.open("GET", url);
+	        request.responseType = "json";
+	        request.send();
+	    };
+
+	    var getPokemonAnimatedSprite = function getPokemonAnimatedSprite(pokemonName) {
+	        //cwkTODO change to passing in pokemon Number and grabbing name for dictionary
+	        //cwkTODO or wrap a pokemon object that has both
+	        var url = "https://www.smogon.com/dex/media/sprites/xy/" + pokemonName + ".gif";
+	        //cwkTODO how to check if image does not exist?
+	        return url;
+	    };
+
+	    //cwkTODO change to passing in the pokemonNumber
+	    var getPokemonSprite = function getPokemonSprite(pokemonName, callback) {
+
+	        //cwkTODO change to passing in pokemonNumber
+	        var animatedSpriteUrl = getPokemonAnimatedSprite(pokemonName);
+	        if (callback) {
+	            callback(animatedSpriteUrl);
+	            return; //cwkTODO instead of doing this, only return if the url is valid
+	        }
+
+	        //cwkTODO update this to v2 now that sprites are supported!
+	        // https://github.com/phalt/pokeapi/issues/80
+	        var url = pokemonApiBasePath + "/api/v1/pokemon/" + pokemonName;
+	        getUrl(url, function (e) {
+	            var pokemon = e.currentTarget.response;
+
+	            if (pokemon && pokemon.sprites && pokemon.sprites.length) {
+	                var spriteUrl = pokemonApiBasePath + pokemon.sprites[0].resource_uri;
+	                getUrl(spriteUrl, function (e) {
+	                    var sprite = e.currentTarget.response;
+
+	                    if (callback) {
+	                        callback(pokemonApiBasePath + sprite.image);
+	                    }
+	                });
+	            } else {
+	                if (callback) {
+	                    // Ref: http://cdn.bulbagarden.net/upload/9/98/Missingno_RB.png
+	                    callback("./missingno.png");
+	                }
+	            }
+	        });
+	    };
+
+	    var getRandomNumberInRangeInclusive = function getRandomNumberInRangeInclusive(min, max) {
+	        // Ref: http://stackoverflow.com/a/7228322
+	        return Math.floor(Math.random() * (max - min + 1) + min);
+	    };
+
+	    var randomPokemon = {
+	        getPokemon: function getPokemon(id, callback, spriteCallback) {
+	            var url = pokemonApiBasePath + "/api/v2/pokemon/" + id;
+	            getUrl(url, function (e) {
+	                var pokemon = e.currentTarget.response;
+
+	                if (callback) {
+	                    callback(pokemon);
+	                }
+
+	                getPokemonSprite(pokemon.name, spriteCallback);
+	            });
+	        },
+
+	        getRandomPokemonNumber: function getRandomPokemonNumber() {
+	            // Pokemon API goes from 1 - 721 (Volcanion)
+	            //cwkTODO get this programmatically
+	            return getRandomNumberInRangeInclusive(1, 721);
+	        }
+	    };
+
+	    window.randomPokemon = randomPokemon;
+	})();
 
 /***/ }
 /******/ ]);
